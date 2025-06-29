e8a20a20bd48521c393489e2ef237264bc0472de
waseem abbas <waseemofficee@gmail.com>
2025-06-29 19:50:04 +0500

    first commit

---
+++ First_App/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import AppPort from './src/AppPort'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <AppPort />
+    </SafeAreaView>
+
+  )
+}
+
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ First_App/src/AppPort.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+const AppPort = () => {
+    return (
+            <View style={styles.container}>
+                <Text style={styles.wightText}>
+                    SafeAreaView
+                </Text>
+            </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    wightText: {
+        color: '#FFFFFF',
+    },
+    blackText: {
+        color: '#000000',
+    }
+});
+
+export default AppPort;
